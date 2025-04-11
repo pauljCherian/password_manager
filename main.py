aeb15d416be3d11b6fc8fb1b931b52da50affd64
Isabelle Han <isabellehan@Isabelles-MacBook-Air-3.local>
2025-04-11 15:50:43 -0400

    added scaffolding

---
+++ main.py
@@ -0,0 +1,20 @@
+# CLI interface 
+import auth
+import crypto_utils 
+
+# functions 
+
+def add_new_credential(service, username, password): 
+    pass 
+
+def search_by_service(service): 
+    pass 
+
+def retrieve_credential(service, username): 
+    pass 
+
+def edit_credential(service, username, new_password): 
+    pass 
+
+def delete_credential(service): 
+    pass
