032e8616354916fc2495a2e117b9717d3ef32b87
Isabelle Han <isabellehan@Isabelles-MacBook-Air-3.local>
2025-04-11 15:17:14 -0400

    added encryption and decryption logic

---
+++ crypto_utils.py
@@ -0,0 +1,22 @@
+# encryption and decryption logic 
+from crytpography.fernet import Fernet
+# Constant encryption_key variable to remember the encryption key 
+# add_encrypted_credential(service, username, password) 
+# calls some encrypt function in cryptography library to encrypt service, username, password 
+# Stores encrypted service, username, password in storage.json
+# decrypt(json object)
+# Use the encryption_key to decrypt and return the username and password 
+
+
+encryption_key = Fernet.generate_key()
+f = Fernet(encryption_key)
+
+# only encrypt a password 
+def encrypt_password(password): 
+    token = f.encrypt(password)
+    return token  #encrypted bit string 
+
+#decrypt some json object password 
+def decrypt_password(encrypted_password): 
+    password = f.decrypt(encrypted_password)
+    return password 
